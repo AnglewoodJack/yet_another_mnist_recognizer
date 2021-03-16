d9c4645a290b2846a1ee153cdf9751b971bbd187
iandryushin <i.andryushin@gmail.com>
2021-03-16 10:25:03 +0300

    benchmark improvement

--- benchmark/keras_mnist.py
+++ benchmark/keras_mnist.py
@@ -1,20 +1,33 @@
 from keras import models
 from keras import layers
+from keras import optimizers
+from keras import callbacks
 from keras.utils import to_categorical
 from keras.datasets import mnist
 
+from pathlib import Path
+
 # Загрузка набора данных MNIST
 from preprocess.deskew import apply_deskew
-
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# Пусть к файлу обученной модели
+path = Path("/Users/ivanandrusin/Desktop/NeuralNetwork/benchmark/model_trained")
+path.mkdir(exist_ok=True, parents=True)
+assert path.exists()
+cpt_filename = "best_checkpoint.hdf5"
+cpt_path =str(path / cpt_filename)
 
 # Архитектура сети
 network = models.Sequential()
 network.add(layers.Dense(800, activation='relu', input_shape=(28 * 28,)))
 network.add(layers.Dense(10, activation='softmax'))
 
+# Оптимизация градиентного спуска
+optimizer = optimizers.Adam(lr=0.001)
+
 # Компиляция
-network.compile(optimizer='rmsprop',
+network.compile(optimizer=optimizer,
 				loss='categorical_crossentropy',
 				metrics=['accuracy'])
 
@@ -26,9 +39,9 @@ test_images = apply_deskew(test_images)
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 
-# Обучение
-network.fit(train_images, train_labels, epochs=20, batch_size=32)
+# запись чекпоинтов
+checkpoint = callbacks.ModelCheckpoint(cpt_path, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
 
-# Предсказание
-test_loss, test_acc = network.evaluate(test_images, test_labels)
-print('accuracy: ', test_acc)
+# Обучение
+network.fit(train_images, train_labels, validation_data=(test_images, test_labels),
+			epochs=20, batch_size=32, verbose=1, callbacks=[checkpoint])
